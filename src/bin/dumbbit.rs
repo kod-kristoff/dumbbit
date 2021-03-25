4f72dd513aa8dd298947899ed730114c3a85a2ce
Kristoffer Andersson <kod.kristoff@gmail.com>
2021-03-25 21:15:33 +0100

    feat: add http server

--- src/bin/dumbbit.rs
+++ src/bin/dumbbit.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("hello world");
+#[async_std::main]
+async fn main() -> anyhow::Result<()> {
+    let mut app = tide::new();
+
+    app.at("/").get(|_| async move { Ok("Hello world!\n") });
+    
+    Ok(app.listen("127.0.0.1:8080").await?)
 }
