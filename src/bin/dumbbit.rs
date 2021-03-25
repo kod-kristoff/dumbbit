2a668ab3e5a21ddbf4ddb5612f28676b42557520
Kristoffer Andersson <kod.kristoff@gmail.com>
2021-03-25 21:25:06 +0100

    style: fix whitespace

--- src/bin/dumbbit.rs
+++ src/bin/dumbbit.rs
@@ -3,6 +3,6 @@ async fn main() -> anyhow::Result<()> {
     let mut app = tide::new();
 
     app.at("/").get(|_| async move { Ok("Hello world!\n") });
-    
+
     Ok(app.listen("127.0.0.1:8080").await?)
 }
